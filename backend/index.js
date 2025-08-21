77f5f55d0fd7f8c6398a7190db1ae019abe96bba
kinteog <67054975+kinteog@users.noreply.github.com>
2025-08-21 20:25:23 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -31,7 +31,7 @@ dotenv.config()
 const app = express()
 const port = process.env.PORT || 8000;
 const corsOptions = {
-  origin:true,
+  origin:"https://garage-frontend-jmak.onrender.com",
   credentials:true
 }
 
